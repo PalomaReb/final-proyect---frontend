ebddf6ad4307dd17af92cd99c5856641c6c5afbb
PalomaReb <paloma.rebolleda@gmail.com>
2021-09-09 10:20:18 +0200

    header modification

--- src/componentes-webpage/header/index.js
+++ src/componentes-webpage/header/index.js
@@ -1,6 +1,7 @@
 import { useStyles } from '../main-style/styles';
 import Button from '../buttons'
 import { Link } from "react-router-dom";
+import { Grid } from '@material-ui/core';
 
 function Header(props) {
     const classes = useStyles();
@@ -8,10 +9,12 @@ function Header(props) {
     return (
         <header className={classes.homeContainer}>
             <nav className={classes.headerContainer}>
-                <Link className={classes.linkStyle} to="/"><Button className={classes.headerLink} color="primary" buttonInfo="Home"></Button></Link>
-                <Link className={classes.linkStyle} to="/register"> <Button className={classes.headerLink} color="primary" buttonInfo="Register"></Button></Link>
-                <Link className={classes.linkStyle} to="/login"> <Button className={classes.headerLink} color="primary" buttonInfo="Log in"></Button></Link>
-                <Link className={classes.linkStyle} to="/review"><Button className={classes.headerLink} color="primary" buttonInfo="Reviews"></Button></Link>
+                <Grid>
+                    <Link to='/'><Button spacing={2} className={classes.headerLink} color="primary" buttonInfo="Home"></Button></Link>
+                    <Link to='/register'><Button className={classes.headerLink} color="primary" buttonInfo="Register"></Button></Link>
+                    <Link to='/login'><Button className={classes.headerLink} color="primary" buttonInfo="Log in"></Button></Link>
+                    <Link to='/reviews'><Button className={classes.headerLink} color="primary" buttonInfo="Reviews"></Button></Link>
+                </Grid>
             </nav>
         </header >
 
