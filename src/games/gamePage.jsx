b54155f60ff92a8f0122e3efd8c99e847a82f9c7
PalomaReb <paloma.rebolleda@gmail.com>
2021-09-10 20:15:32 +0200

    game compoent

--- src/games/gamePage.jsx
+++ src/games/gamePage.jsx
@@ -4,27 +4,42 @@ import Button from "../componentes-webpage/buttons/index";
 import Header from "../componentes-webpage/header";
 import Footer from "../componentes-webpage/footer";
 import { useStyles } from "./backgroundImages";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 function GamePage() {
+  const { id } = useParams(); //aqui se crea una variable que recoge el ID del path param
+  const [gameInfo, setGameInfo] = useState({}); //use state para pintar lo que se va a recoger de mongo
+  useEffect(() => {
+    // para que se rederize el juego cuando se refresca la pagina
+    fetch(`http://localhost:5463/games/${id}`) //llama al param "game" por id de juego
+      .then((r) => r.json()) //promesa que devuelve el json
+      .then((data) => {
+        setGameInfo(data);
+        console.log(data); //promesa que busca los datos de game y usa el setGameInfo para redendizar la pagina
+      }); //pinta solo el id del juego
+  }, [id]);
+
+  // setTimeout(function () {
+  //   window.location.href = "http://localhost:3000/death";
+  // }, 3000);
+
   const classes = useStyles();
+
   return (
     <React.Fragment>
-      <Grid item xs={12}>
-        <Header></Header>
-      </Grid>
+      <Header></Header>
       <Grid item xs={12} className={classes.imgForGame}>
         <Grid item xs={12} className={classes.backgroundGame}>
-          <Typography className={classes.timer} color="primary">
-            Esto es el timer del juego
-          </Typography>
+          <Typography className={classes.timer} color="primary"></Typography>
         </Grid>
         <Grid item xs={12} md={7}>
           <Typography variant="h2" color="primary" className={classes.acertijo}>
-            Los errores no siempre tienen porque ser malos. A veces hay que
-            tener un error y enfrentarte a el antes de poder seguir adelante. Si
-            quieres poder pasar este nivel. Tendras que buscar el error que en
-            la web no se puede encontrar. Ten cuidado con lo que haces, no
-            tienes mucho tiempo.
+            {gameInfo?.instructions?.body[0]}
+            {gameInfo?.instructions?.body[1]}
+            {gameInfo?.instructions?.body[2]}
+            {gameInfo?.instructions?.body[3]}
+            {gameInfo?.instructions?.body[4]}
           </Typography>
         </Grid>
         <Grid item xs={12} md={7} className={classes.inputCenter}>
@@ -34,15 +49,12 @@ function GamePage() {
             type="password"
             name="answer"
             label="RESPUESTA"
-            // ref={passwordRef}
             variant="outlined"
           />
           <Button color="primary" buttonInfo="ESCAPAR?????"></Button>
         </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Footer></Footer>
-      </Grid>
+      <Footer></Footer>
     </React.Fragment>
   );
 }
