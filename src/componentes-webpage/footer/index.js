ae025c65826ee4bf1bbb5cf4dcf10e409e298715
PalomaReb <paloma.rebolleda@gmail.com>
2021-09-10 20:16:37 +0200

    footer with grid

--- src/componentes-webpage/footer/index.js
+++ src/componentes-webpage/footer/index.js
@@ -1,17 +1,17 @@
 import { useStyles } from '../main-style/styles';
 import Button from '../buttons'
+import { Container, Grid } from '@material-ui/core'
 
 function Footer(props) {
     const classes = useStyles();
     return (
-        <div className={classes.homeContainer}>
-            <Button color="primary" buttonInfo="Legal Terms"></Button>
-            <Button color="primary" buttonInfo="Cookies"></Button>
-            <Button color="primary" buttonInfo="General Data Protection Regulation"></Button>
-
-
-
-        </div>
+        <Container>
+            <Grid container xs={12} className={classes.homeContainer}>
+                <Button color="primary" buttonInfo="Legal Terms"></Button>
+                <Button color="primary" buttonInfo="Cookies"></Button>
+                <Button color="primary" buttonInfo="GDPR"></Button>
+            </Grid>
+        </Container>
     );
 
 }
