ca2e74edccdd37c3bd1c798cf9d6c8654533cab5
PalomaReb <paloma.rebolleda@gmail.com>
2021-09-10 20:16:50 +0200

    header with grid

--- src/componentes-webpage/header/index.js
+++ src/componentes-webpage/header/index.js
@@ -1,22 +1,26 @@
 import { useStyles } from '../main-style/styles';
 import Button from '../buttons'
 import { Link } from "react-router-dom";
-import { Grid } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 
 function Header(props) {
     const classes = useStyles();
 
     return (
-        <header className={classes.homeContainer}>
-            <nav className={classes.headerContainer}>
-                <Grid>
-                    <Link className={classes.headerLink} to='/'><Button spacing={2} className={classes.headerLink} color="primary" buttonInfo="Home"></Button></Link>
-                    <Link className={classes.headerLink} to='/register'><Button className={classes.headerLink} color="primary" buttonInfo="Register"></Button></Link>
-                    <Link to='/login'><Button className={classes.headerLink} color="primary" buttonInfo="Log in"></Button></Link>
-                    <Link to='/reviews'><Button className={classes.headerLink} color="primary" buttonInfo="Reviews"></Button></Link>
-                </Grid>
-            </nav>
-        </header >
+        <Container>
+            <header className={classes.homeContainer}>
+
+                <nav className={classes.headerContainer}>
+                    <Grid>
+                        <Link className={classes.headerLink} to='/'><Button spacing={2} className={classes.headerLink} color="primary" buttonInfo="Home"></Button></Link>
+                        <Link className={classes.headerLink} to='/register'><Button className={classes.headerLink} color="primary" buttonInfo="Register"></Button></Link>
+                        <Link to='/login'><Button className={classes.headerLink} color="primary" buttonInfo="Log in"></Button></Link>
+                        <Link to='/reviews'><Button className={classes.headerLink} color="primary" buttonInfo="Reviews"></Button></Link>
+                    </Grid>
+                </nav>
+
+            </header >
+        </Container>
 
 
     );
