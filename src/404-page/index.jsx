c4b6b458c89d3a8a3088dcccdd3c6925c3d11863
PalomaReb <paloma.rebolleda@gmail.com>
2021-09-10 20:11:18 +0200

    404 page modifications

--- src/404-page/index.jsx
+++ src/404-page/index.jsx
@@ -1,7 +1,9 @@
 import { useStyles } from "../404-page/styles";
 import React from "react";
-import { Typography } from "@material-ui/core";
-import Button from "../componentes-webpage/buttons";
+import Header from "../componentes-webpage/header/index";
+import Footer from "../componentes-webpage/footer/index";
+import { Container, Typography, Button } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 function NotFound() {
@@ -9,32 +11,47 @@ function NotFound() {
 
   return (
     <React.Fragment>
-      <div className={classes.crazy}>
-        <div className={classes.center}>
-          <Typography variant="h1" color="primary">
-            404! This is not the exit you are looking for.
-          </Typography>
-        </div>
-        <div className={classes.baseText}>
-          <Typography color="secondary" className={classes.center}>
-            aWYgeW91IGFyZSBoZXJlLCB5b3UgaGF2ZSBtYWRlIGl0IHZlcnkgZmFy
-          </Typography>
-          <Typography color="secondary" className={classes.center}>
-            YnV0IHRoYXQgZG9lcyBub3QgbWVhbiB5b3UgYXJlIHNhZmU=
-          </Typography>
-          <Typography color="secondary" className={classes.center}>
-            b25seSBieSBrbm93aW5nIHdoYXQgdGhpcyBmb3JtYXQgaXM={" "}
-          </Typography>
-          <Typography color="secondary" className={classes.center}>
-            d2lsbCB5b3UgYmUgYWJsZSB0byBjb21wbGV0ZSB0aGlzIGxldmVs
-          </Typography>
-        </div>
-      </div>
-      <div>
-        <Link to="/">
-          <Button buttonInfo="back home"> </Button>
-        </Link>
-      </div>
+      <Header></Header>
+
+      <Grid className={classes.crazy}>
+        <Container>
+          <Grid container className={classes.fourofour}>
+            <Grid item xs={12} md={5} className={classes.title}>
+              <Typography variant="h1" color="primary">
+                404! This is not the exit you are looking for. You have the
+                wrong code.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={3} className={classes.baseText}>
+              <Typography color="secondary" className={classes.center}>
+                aWYgeW91IGFyZSBoZXJlLCB5b3UgaGF2ZSBtYWRlIGl0IHZlcnkgZmFy
+              </Typography>
+              <Typography color="secondary" className={classes.center}>
+                YnV0IHRoYXQgZG9lcyBub3QgbWVhbiB5b3UgYXJlIHNhZmU=
+              </Typography>
+              <Typography color="secondary" className={classes.center}>
+                b25seSBieSBrbm93aW5nIHdoYXQgdGhpcyBmb3JtYXQgaXM={" "}
+              </Typography>
+              <Typography color="secondary" className={classes.center}>
+                d2lsbCB5b3UgYmUgYWJsZSB0byBjb21wbGV0ZSB0aGlzIGxldmVs
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Link to="/">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.FoFbtn}
+                >
+                  Go Home
+                </Button>
+              </Link>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grid>
+
+      <Footer></Footer>
     </React.Fragment>
   );
 }
--- src/404-page/styles.js
+++ src/404-page/styles.js
@@ -4,17 +4,37 @@ import clownOnWheel from '../assets/images/clownOnWheels.jpg'
 export const useStyles = makeStyles((theme) => ({
     crazy: {
         background: `url(${clownOnWheel})`,
-        backgroundPosition: "center",
+        backgroundPosition: "center top",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        height: "100vh"
+        minHeight: "70vh",
     },
+
+    fourofour: {
+        width: "100%",
+        justifyContent: "space-between",
+        padding: "2.7rem 2rem",
+    },
+
     center: {
         textAlign: "center",
-        color: "#20130a"
+        color: "#20130a",
+        overflowWrap: "break-word",
+        opacity: ".5",
+        '&:hover': {
+            cursor: "default",
+        },
+        [theme.breakpoints.down('sm')]: {
+            opacity: ".2",
+        }
+
     },
-    baseText: {
 
-    }
+    FoFbtn: {
+        margin: "1rem 2rem",
+    },
 
+    title: {
+        padding: "2rem",
+    },
 }));
