07fc99f7393b12bd58a18a59f8a05394f3f76d58
PalomaReb <paloma.rebolleda@gmail.com>
2021-09-21 11:36:03 +0200

    vh

--- src/componentes-webpage/registerPage/styles/styles.js
+++ src/componentes-webpage/registerPage/styles/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme) => ({
     backgroundContainer: {
         backgroundColor: "#908b89",
-        height: "80vh"
+        height: "75vh"
     },
     userInputs: {
         width: "100%",
