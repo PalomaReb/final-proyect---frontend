f41a71eeba378c75964968af905e2918730099d2
PalomaReb <paloma.rebolleda@gmail.com>
2021-10-18 18:06:27 +0200

    cambios fetch

--- src/pages/how-it-works/index.jsx
+++ src/pages/how-it-works/index.jsx
@@ -37,7 +37,7 @@ function HowItWorks() {
           if (r) r.json();
         })
         .then((data) => {
-          console.log(data.gameList.length);
+          // console.log(data.gameList.length);
           if (data && data?.gameList.length > 0) {
             if (data?.gameList[data?.gameList.length - 1].status === "dead") {
               history.push(
